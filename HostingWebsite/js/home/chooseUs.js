b52cabf2ade0683e8523a16b503cd84f9d41d3cd
BuildTools <unconfigured@null.spigotmc.org>
2023-04-24 16:45:48 +0700

    des+dec_xongChooseUs

---
+++ HostingWebsite/js/home/chooseUs.js
@@ -0,0 +1,12 @@
+var getChooseUs = document.getElementsByClassName("chooseUs")[0];
+
+
+window.addEventListener("scroll", setClassAnimation);
+window.addEventListener("load", setClassAnimation);
+
+function setClassAnimation(){
+    if(getGetStarted.getBoundingClientRect().top < window.innerHeight) {
+        getChooseUs.classList.add("chooseUs--animation");
+        getChooseUs.classList.add("chooseUs--animation");
+    }
+}

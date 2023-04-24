480fde5bad2015b0e5a4825b0be7360f8253b2d9
BuildTools <unconfigured@null.spigotmc.org>
2023-04-24 17:17:38 +0700

    des+dec_xongBuildingWebsite

--- HostingWebsite/js/home/chooseUs.js
+++ HostingWebsite/js/home/chooseUs.js
@@ -5,7 +5,7 @@ window.addEventListener("scroll", setClassAnimation);
 window.addEventListener("load", setClassAnimation);
 
 function setClassAnimation(){
-    if(getGetStarted.getBoundingClientRect().top < window.innerHeight) {
+    if(getChooseUs.getBoundingClientRect().top < window.innerHeight) {
         getChooseUs.classList.add("chooseUs--animation");
         getChooseUs.classList.add("chooseUs--animation");
     }
